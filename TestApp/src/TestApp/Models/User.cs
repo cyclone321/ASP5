2b378e5d14c22eabcb486c60b0179cf3c0467bc5
johann.combrink <johhann.combrink@thumbtribe.com>
2016-01-05 08:29:34 +0200

    Added User to try and Scaffold

---
+++ TestApp/src/TestApp/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestApp.Models
+{
+    public class User
+    {
+            [Required]
+            [MinLength(4)]
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public int Age { get; set; }
+            [Required]
+            [FileExtensions(Extensions = "jpg,jpeg")]
+            public IFormFile File { get; set; }
+
+        }
+    }
